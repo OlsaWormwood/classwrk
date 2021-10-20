5e1f502441531ed47040c14d36de6fc91b64dd8e
v.ivanitski <v.ivanitski@DESKTOP-7GTAVK3>
2021-10-20 21:55:52 +0300

    Добавьте файлы проекта.

---
+++ classwrk/Bar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classwrk
+{
+   public class Bar
+    {
+        public string Name { get; set; }
+        public List<Visitor> Visitors { get; set; } 
+    }
+}
---
+++ classwrk/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace classwrk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bar = new Bar() { Name = "Hyena" };
+            bar.Visitors = new List<Visitor>();
+            bar.Visitors.Add(new Stalker() { Name = "Hvosch", GroupName = "Svoboda" });
+            bar.Visitors.Add(new Biker() { Name = "Ruslik", MotorbikeName = "Purple rocket" });
+            bar.Visitors.Add(new Tolchok() { Name = "Dexter", GameName = "Warhammer FB" });
+            bar.Visitors.Add(new Visitor() { Name = "Razor"});
+
+            foreach (var visitor in bar.Visitors)
+            {
+                visitor.WhatHesDoing();
+            }
+
+        }
+    }
+}
